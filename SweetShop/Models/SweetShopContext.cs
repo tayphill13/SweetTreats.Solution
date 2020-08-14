f046798e1af84e95b7e7f78e164d8d0bc74cddcf
Taylor Phillips <taylorphillips133@gmail.com>
2020-08-14 09:30:36 -0700

    complete the SweetShopContext file

---
+++ SweetShop/Models/SweetShopContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SweetShop.Models
+{
+  public class SweetShopContext : IdentityDbContext<ApplicationUser>
+  {
+    public virtual DbSet<Flavor> Flavors { get; set; }
+    public virtual DbSet<Treat> Treats { get; set; }
+    public DbSet<TreatFlavor> TreatFlavor { get; set; }
+
+    public SweetShopContext(DbContextOptions options) : base(options) {}
+  }
+}
