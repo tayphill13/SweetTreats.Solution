717bb768b16a9af352e59339d20616a2a75d6048
Taylor Phillips <taylorphillips133@gmail.com>
2020-08-14 09:15:09 -0700

    complete the Treat.cs class file, setting up relationship with flavor

---
+++ SweetShop/Models/Treat.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace SweetShop.Models
+{
+  public class Treat
+  {
+    public Treat()
+    {
+      this.Flavors = new HashSet<TreatFlavor>();
+    }
+
+    public int TreatId  { get; set; }
+    public string Name { get; set; }
+    public virtual ApplicationUser User { get; set; }
+    public virtual ICollection<TreatFlavor> Flavors { get; set; }
+  }
+}
