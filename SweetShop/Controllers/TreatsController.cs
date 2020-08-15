0303631dc7ac9b78eb4ddea81d1b4a6ea416c8f4
Taylor Phillips <taylorphillips133@gmail.com>
2020-08-15 16:23:33 -0700

    adjust the TreatsController to allow for users to see index and details pages with authentication

--- SweetShop/Controllers/TreatsController.cs
+++ SweetShop/Controllers/TreatsController.cs
@@ -21,12 +21,10 @@ namespace SweetShop.Controllers
       _userManager = userManager;
       _db = db;
     }
-    public async Task<ActionResult> Index()
+    public ActionResult Index()
     {
-      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-      var currentUser = await _userManager.FindByIdAsync(userId);
-      var userTreats = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).ToList();
-      return View(userTreats);
+      List<Treat> model = _db.Treats.ToList();
+      return View(model);
     }
 
     [Authorize]
